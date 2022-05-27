0b0b6331ba950569c57190aea8203ff3973e24e3
Luc Weinbrecht <luc.weinbrecht@novatec-gmbh.de>
2022-05-27 06:15:54 +0200

    setting business key for loan agreement

--- loan-agreement/src/test/java/de/weinbrecht/luc/bpm/architecture/loan/agreement/adapter/in/process/SendCrossSellingRecommendationTest.java
+++ loan-agreement/src/test/java/de/weinbrecht/luc/bpm/architecture/loan/agreement/adapter/in/process/SendCrossSellingRecommendationTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import java.util.HashMap;
 import java.util.Map;
 
+import static de.weinbrecht.luc.bpm.architecture.loan.agreement.adapter.common.ProcessConstants.BUSINESS_KEY;
 import static de.weinbrecht.luc.bpm.architecture.loan.agreement.adapter.common.ProcessConstants.LOAN_AGREEMENT_NUMBER;
 import static de.weinbrecht.luc.bpm.architecture.loan.agreement.domain.model.TestdataGenerator.createLoanAgreementWithNumber;
 import static org.mockito.Answers.RETURNS_DEEP_STUBS;
@@ -34,11 +35,12 @@ class SendCrossSellingRecommendationTest {
     @Test
     void should_load_data_and_start_process_by_message() {
         LoanAgreement loanAgreement = createLoanAgreementWithNumber();
-        String caseId = "11";
+        CaseId caseId = new CaseId("11");
         JobClient client = mock(JobClient.class, RETURNS_DEEP_STUBS);
         ActivatedJob job = mock(ActivatedJob.class);
         Map<String, Object> variables = new HashMap<>();
         variables.put(LOAN_AGREEMENT_NUMBER, loanAgreement.getLoanAgreementNumber().getValue().intValue());
+        variables.put(BUSINESS_KEY, caseId.getValue());
         when(job.getVariablesAsMap()).thenReturn(variables);
         when(job.getKey()).thenReturn(1L);
         when(loanAgreementQuery.loadByNumber(loanAgreement.getLoanAgreementNumber())).thenReturn(loanAgreement);
@@ -47,6 +49,6 @@ class SendCrossSellingRecommendationTest {
 
         verify(client.newCompleteCommand(job.getKey())).send();
 
-        verify(recommendationTrigger).startLoanAgreement(new CaseId(caseId), loanAgreement);
+        verify(recommendationTrigger).startLoanAgreement(caseId, loanAgreement);
     }
 }
